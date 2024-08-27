3469470305c55bf2c3aaa4f4acb34a376c24359b
Hamza-Doubaa1 <douhy002@mymail.unisa.edu.au>
2024-08-27 12:39:03 +0930

    Starter files
    Recently moved houses and I just got internet a few days ago couldn't commit any work that time.

---
+++ starter files/main.py
@@ -0,0 +1,8 @@
+"""
+File: main.py
+Description: <A brief description of this Python module.>
+Author: <Hamza Doubaa>
+ID: <110445450>
+Username: <douhy002>
+This is my own work as defined by the University's Academic Misconduct Policy.
+"""
